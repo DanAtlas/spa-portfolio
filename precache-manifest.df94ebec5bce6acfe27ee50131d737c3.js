78a8217061eada0161078dc7746f854857168693
DanAtlas <abishev295@gmail.com>
2019-11-19 00:06:38 +0600

    Updates

---
+++ precache-manifest.df94ebec5bce6acfe27ee50131d737c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d26e8344c4f3801b4c4ccef848e7fd3b",
+    "url": "/react-spa/static/media/man.d26e8344.svg"
+  },
+  {
+    "revision": "29c77d414629407bfee1008ad5c58fc6",
+    "url": "/react-spa/static/media/Trees.29c77d41.svg"
+  },
+  {
+    "revision": "8e8fa4a82b4afa61763bb7a14027a815",
+    "url": "/react-spa/static/media/Stars.8e8fa4a8.svg"
+  },
+  {
+    "revision": "3e306d59b7e4046b4373",
+    "url": "/react-spa/static/js/runtime~main.97fb65bb.js"
+  },
+  {
+    "revision": "24f640093241250a04cc",
+    "url": "/react-spa/static/js/main.bf9e47d7.chunk.js"
+  },
+  {
+    "revision": "eac251ae79934baf5b7d",
+    "url": "/react-spa/static/js/2.60eec839.chunk.js"
+  },
+  {
+    "revision": "24f640093241250a04cc",
+    "url": "/react-spa/static/css/main.df9f09b5.chunk.css"
+  },
+  {
+    "revision": "18f89903a01b2045b8bb019e54f4e2de",
+    "url": "/react-spa/index.html"
+  }
+];
--- src/components/skills.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Skills = (props) => (
-  <div className="main-content">
-    <h2 className="main-content__title wow fadeInDown" data-wow-duration="1s">{props.title}</h2>
-  </div>
-);
-
-export default Skills;
